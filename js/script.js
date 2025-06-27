a62a1e3238b4d7c70aa1adbd123892536e2961c4
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-06-27 21:33:29 +0200

    com9

--- js/script.js
+++ js/script.js
@@ -14,3 +14,43 @@ window.addEventListener("scroll", function () {
     button.classList.remove("btn-scrolled");
   }
 });
+
+const mArray = document.querySelectorAll("svg g");
+/*
+const randomM = function () {
+  for (i = 0; i < mArray.length; i++) {
+    let r = Math.floor(Math.random() + 0.1);
+    if (r === 1) {
+      mArray[i].style.opacity = "1";
+      console.log(r);
+    } else {
+      mArray[i].style.opacity = "0";
+      console.log(r);
+    }
+  }
+}; */
+
+//randomM();
+
+//setInterval(randomM, 500);
+
+const ran = function () {
+  let r = Math.random();
+  if (r <= 0.85) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const randomM = function () {
+  for (i = 0; i < mArray.length; i++) {
+    if (ran()) {
+      mArray[i].style.opacity = "1";
+    } else {
+      mArray[i].style.opacity = "0";
+    }
+  }
+};
+
+setInterval(randomM, 500);
